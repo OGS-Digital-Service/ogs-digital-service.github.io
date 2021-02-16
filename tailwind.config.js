070c7ecf25a5d1669dc7370b8e007ecdbcbc859b
Jeff HS K <58082485+knaackbuilt@users.noreply.github.com>
2021-02-16 16:15:31 -0500

    added event submission and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'DOL-theme-primary': '#0b5d66',
+        'DOL-theme-darker':'#073d42',
+      }
+
+    },
+    
   },
   variants: {
     extend: {},
