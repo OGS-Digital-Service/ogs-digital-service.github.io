a16cc7013c87d12849369d0fd399e1edc1e3c42f
Jeff Knaack <58082485+knaackbuilt@users.noreply.github.com>
2025-08-08 16:22:23 -0400

    add documents folder and a11y document

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,7 @@ module.exports = function(eleventyConfig){
       eleventyConfig.addPassthroughCopy("fonts");
       eleventyConfig.addPassthroughCopy("js");
       eleventyConfig.addPassthroughCopy("css");
+      eleventyConfig.addPassthroughCopy("documents");
     
 
     return {
