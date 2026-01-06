f989c829fa6a223067717fb6cffa8dcf69df387f
Jeff Knaack <58082485+knaackbuilt@users.noreply.github.com>
2026-01-06 16:58:27 -0500

    install ICS module and create 2026 event

---
+++ _calendar/sots2026.js
@@ -0,0 +1,24 @@
+const { writeFileSync } = require('fs')
+const ics = require('ics')
+
+ics.createEvent({
+  start: [2026, 1, 13, 13, 0],
+  duration: { hours: 1, minutes: 0 },
+  title: "Gov. Hochul's 2026 State of the State Address",
+  description: "On January 13, 2026, Governor Hochul is delivering her 2026 State of the State Address! Tune in to hear the governor's plans to make New York safer, more affordable, and a place where every family can thrive.",
+  location: 'Virtual',
+  url: 'https://www.governor.ny.gov/'
+}, (error, value) => {
+  if (error) {
+    console.log(error)
+  }
+
+  writeFileSync(`./calendar/SOTS2026.ics`, value)
+
+/*
+You cannot use fs in Frontend libraries like React so you rather import a module to save files to the browser as follow [  import { saveAs } from 'file-saver'; // For saving the file in the browser]
+const blob = new Blob([value], { type: 'text/calendar' });
+        saveAs(blob, `${title}.ics`);
+
+*/
+})
